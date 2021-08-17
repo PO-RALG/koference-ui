26271e70914241a4e738447f6db0e6d28e252a60
Zachariah Ngonyani <zachariahn@gmail.com>
2021-08-17 21:22:30 +0300

    create the home component

---
+++ src/layouts/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app class="grey light-4">
+    <v-content class="mx-4">
+      <router-view></router-view>
+    </v-content>
+    <Landing />
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Landing from "@/layouts/Landing.vue";
+
+export default Vue.extend({
+  components: {
+    Landing,
+  },
+  data: () => ({ drawer: null }),
+});
+</script>
