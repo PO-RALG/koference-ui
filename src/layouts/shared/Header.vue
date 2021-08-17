860513539f0518e466f8a23db905638e296aa6b9
Zachariah Ngonyani <zachariahn@gmail.com>
2021-08-17 21:23:34 +0300

    create the header component

---
+++ src/layouts/shared/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app-bar app color="primary">
+    <v-app-bar-nav-icon
+      v-if="!drawer"
+      @click.stop="toggleSidebar"
+      color="white"
+      large
+    />
+    <v-toolbar-title class="header-toolbar"></v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    logoutFunction: {
+      type: Function,
+      required: false,
+    },
+    sidebarToggle: {
+      type: Function,
+      required: false,
+    },
+    drawer: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    // methods
+    const toggleSidebar = () => {
+      context.emit("sidebarToggle", props.drawer);
+    };
+
+    const logout = () => {
+      context.emit("logoutFunction", true);
+    };
+
+    return {
+      toggleSidebar,
+      logout,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.header-toolbar {
+  color: #fff;
+  background-color: rgb(33,150,243) !important;
+}
+</style>
