0f93752d897d38a430135c0b11086da10e19c530
Zachariah Ngonyani <zachariahn@gmail.com>
2021-08-17 21:55:14 +0300

    Add and configure axios for http requests

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 import PerfectScrollbar from "vue2-perfect-scrollbar";
 import VueCompositionAPI from "@vue/composition-api";
 
+axios.defaults.headers.common["Accept"] = `application/json`;
+axios.defaults.headers.common["Content-Type"] = `application/json`;
+axios.defaults.baseURL = process.env.VUE_APP_SERVER_URL;
+
 Vue.use(VueCompositionAPI);
 Vue.use(PerfectScrollbar);
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
