a7a185b0f6cf2295b4a86b30c82714204f88ac28
Zachariah Ngonyani <zachariahn@gmail.com>
2021-08-20 09:11:46 +0300

    WIP: Add reusable data table

---
+++ src/components/shared/DataTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="dataNumbers"
+      :rows-per-page-items="rowsPerPageItems"
+      :search="search"
+      :custom-filter="customFilter"
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.name }}</td>
+        <td>{{ props.item.calories }}</td>
+        <td>{{ props.item.fat }}</td>
+        <td>{{ props.item.carbs }}</td>
+        <td>{{ props.item.protein }}</td>
+        <td>{{ props.item.iron }}</td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
