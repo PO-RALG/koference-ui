f7604c227ad75505a5e083d11ceb81aa9134cbca
Zachariah Ngonyani <zachariahn@gmail.com>
2021-09-11 18:35:56 +0300

    add snackbar in store

---
+++ src/components/shared/SnackBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-snackbar
+      v-for="(snackbar, index) in snackbars.filter((s) => s.showing)"
+      :key="snackbar.text + Math.random()"
+      v-model="snackbar.showing"
+      :timeout="snackbar.timeout"
+      :color="snackbar.color"
+      :style="`bottom: ${index * 60 + 8}px`"
+    >
+      {{ snackbar.text }}
+
+      <v-btn text @click="snackbar.showing = false"> Close </v-btn>
+    </v-snackbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "@vue/composition-api";
+import store from "@/store";
+
+export default defineComponent({
+  setup() {
+    const snackbars = computed(() => store.state.snackbar);
+
+    return {
+      snackbars,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/store/modules/Snackbar.ts
@@ -0,0 +1,31 @@
+const state = {
+  snackbars: [],
+};
+
+const getters = {
+  getSnackBar: (state) => {
+    return state.snackbar;
+  },
+};
+
+const actions = {
+  setSnackbar({ commit }, snackbar) {
+    snackbar.showing = true;
+    snackbar.color = snackbar.color || "success";
+    commit("SET_SNACKBAR", snackbar);
+  },
+};
+
+const mutations = {
+  SET_SNACKBAR(state, snackbar) {
+    state.snackbars = state.snackbars.concat(snackbar);
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+}
