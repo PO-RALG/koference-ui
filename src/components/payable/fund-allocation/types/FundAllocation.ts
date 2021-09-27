d072400d7989bde62b14da77ba5bf9af2d33a9f9
heri matwi <herij26@gmail.com>
2021-09-27 13:27:46 +0300

    fix warnings

--- src/components/payable/fund-allocation/types/FundAllocation.ts
+++ src/components/payable/fund-allocation/types/FundAllocation.ts
@@ -6,3 +6,14 @@ export interface FundAllocation {
   allocation_amount: number;
   facility_id: number;
 }
+
+export interface Budget {
+  id: number;
+  amount: string;
+  activity_id: number;
+  funding_source_id: number;
+  allocation_amount: string;
+  facility_id: number;
+  total_allocated: string;
+  activity: string;
+}
