8f4809cc53f8aa9ab00ef6cfb74ec8d75e6a3db4
Zachariah Ngonyani <zachariahn@gmail.com>
2021-10-15 13:22:52 +0300

    fix padding in login slider

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,15 +1,10 @@
 <template>
-  <v-app
-    justify-center
-    class="grey lighten-3 pa-2 text-center main-body"
-    fluid
-    fill-height
-  >
+  <v-app justify-center class="grey lighten-3 pa-2 text-center main-body" fluid fill-height>
     <v-layout justify-center align-center class="body_bg">
       <v-main align="center" justify="center">
         <v-row align="center" justify="center">
           <v-col cols="8" sm="8" md="7">
-            <v-card height="fit" class="elevation-5">
+            <v-card height="fit" class="elevation-5 pa-0">
               <v-row dense class="mr-0 ml-0" v-if="data.slides.length > 0">
                 <!-- class="d-none d-md-flex d-lg-none d-none d-lg-flex d-xl-none d-none " -->
                 <v-flex grow md6>
@@ -20,14 +15,8 @@
                       interval="10000"
                       height="100%"
                       light
-                      hide-delimiter-background
-                      show-arrows-on-hover
                     >
-                      <v-carousel-item
-                        v-for="(item, i) in data.slides"
-                        :key="i"
-                        :src="item.src"
-                      ></v-carousel-item>
+                      <v-carousel-item v-for="(item, i) in data.slides" :key="i" :src="item.src"></v-carousel-item>
                     </v-carousel>
                   </v-flex>
                 </v-flex>
@@ -48,12 +37,8 @@
                       You must be logged in to access
                       {{ $route.query.redirect }}
                     </v-alert>
-                    <h2 class="text-center pa-6 login-header" color="primary">
-                      LOGIN to your account
-                    </h2>
-                    <h4 class="siteName pa-0 pb-4" v-if="isDemo()">
-                      ({{ data.siteName }})
-                    </h4>
+                    <h2 class="text-center pa-6 login-header" color="primary">LOGIN to your account</h2>
+                    <h4 class="siteName pa-0 pb-4" v-if="isDemo()">({{ data.siteName }})</h4>
                     <v-form ref="form" v-model="data.valid">
                       <v-text-field
                         prepend-inner-icon="mdi-account-box"
@@ -78,16 +63,9 @@
                     </v-form>
                   </v-card-text>
                   <v-card-actions class="mr-3 ml-3 pr-6 pl-6 pt-0">
-                    <small class="text-center mt-3">
-                      <a href="#">FOGOT PASSWORD?</a></small
-                    >
+                    <small class="text-center mt-3"> <a href="#">FOGOT PASSWORD?</a></small>
                     <v-spacer></v-spacer>
-                    <v-btn
-                      color="primary"
-                      class="white--text"
-                      @click="login"
-                      :loading="data.loading"
-                    >
+                    <v-btn color="primary" class="white--text" @click="login" :loading="data.loading">
                       <v-icon left>mdi-login</v-icon>LOGIN
                     </v-btn>
                   </v-card-actions>
@@ -126,9 +104,7 @@ export default Vue.extend({
       email: "",
       emailRules: [
         (v: any) => !!v || "Email is required",
-        (v: any) =>
-          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-          "E-mail must be a valid email",
+        (v: any) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || "E-mail must be a valid email",
       ],
       password: "",
       passwordRules: [(v: any) => !!v || "Password is required"],
@@ -166,7 +142,7 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped>
+<style>
 .coat {
   width: 100px;
   height: 100px;
@@ -179,6 +155,9 @@ export default Vue.extend({
   background-size: cover !important;
   background-repeat: no-repeat !important;
   background-position: center center !important; /* optional, center the image */
+}
+.v-responsive__sizer {
+  padding-bottom: 85% !important;
 }
 .wrapper {
   padding: 0 !important;
