69a858edf4dd50f81d6888c16d6caa3e9c0adc87
Zachariah Ngonyani <zachariahn@gmail.com>
2021-10-19 12:22:00 +0300

    return accidentally deleted file

---
+++ src/components/shared/index.ts
@@ -0,0 +1,17 @@
+import Vue from "vue";
+
+Vue.component("TreeBrowser", () => import("./TreeBrowser.vue"));
+Vue.component("Tree", () => import("./Tree.vue"));
+Vue.component("DualMultiSelect", () => import("./DualMultiSelect.vue"));
+Vue.component("Modal", () => import("./modal/Modal.vue"));
+Vue.component("ModalBody", () => import("./modal/ModalBody.vue"));
+Vue.component("ModalHeader", () => import("./modal/ModalHeader.vue"));
+Vue.component("ModalFooter", () => import("./modal/ModalFooter.vue"));
+Vue.component("Snackbar", () => import("../../utils/SnackBar.vue"));
+Vue.component("ConfirmDialog", () => import("./ConfirmDialog.vue"));
+Vue.component("Paginate", () => import("./Paginate.vue"));
+Vue.component("SearchField", () => import("./SearchField.vue"));
+Vue.component("DatePicker", () => import("./DatePicker.vue"));
+Vue.component("BaseErrorMessage", () => import("./form/BaseErrorMessage.vue"));
+Vue.component("Loader", () => import("./Loader.vue"));
+Vue.component("AppLocationHeader", () => import("./AppLocationHeader.vue"));
