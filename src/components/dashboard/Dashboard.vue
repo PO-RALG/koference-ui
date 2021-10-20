67d1960b5b7fa85b5d569816dc0be2f250e4e0db
Zachariah Ngonyani <zachariahn@gmail.com>
2021-10-20 13:52:07 +0300

    Create dashboard component

---
+++ src/components/dashboard/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="academic-year">
+    <v-card-actions class="pa-0">
+      <h2>Dashboard</h2>
+      <v-spacer></v-spacer>
+    </v-card-actions>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+.tree-title {
+  padding: 0 0 5px 0;
+}
+</style>
