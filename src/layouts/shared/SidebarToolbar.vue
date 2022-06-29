6bc502f48b06c95e4ade58d5172a7034bb12703d
Zachariah Ngonyani <zachariahn@gmail.com>
2022-06-29 07:00:38 +0300

    Move sidebar top toolbar with close icon to own separate component

---
+++ src/layouts/shared/SidebarToolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-toolbar-title
+    color="primary"
+    class="sidebar-toolbar d-flex flex-row justify-space-between"
+  >
+    <h3 class="admin-title">FFARS</h3>
+    <v-icon
+      large
+      @click.stop="$emit('onSidebarClose')"
+      color="white"
+      class="sidebar-close-icon"
+      >mdi-close</v-icon
+    >
+  </v-toolbar-title>
+</template>
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  props: {
+    onSidebarClose: {
+      type: Function,
+    },
+  },
+});
+</script>
