b1731959988a54e6ed571258da926b1986992b0d
Lucas Gervas <lucagvas@gmail.com>
2022-09-07 16:08:08 +0300

    fix issue of download of files

--- src/components/setup/document/Document.vue
+++ src/components/setup/document/Document.vue
@@ -45,7 +45,7 @@
                 v-bind="attrs"
                 v-on="on"
                 class="mr-2"
-                @click="downloadFile(item.link)"
+                @click="downloadFile(item.id)"
               >
                 mdi-download
               </v-icon>
