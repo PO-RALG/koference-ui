93be918c60eb5ac4af5d6a01a90ea5c25267b17e
Zachariah Ngonyani <zachariahn@gmail.com>
2022-10-23 09:10:37 +0300

    add gzip and brotli compression during build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vite";
 import Components from "unplugin-vue-components/vite";
 import { VuetifyResolver } from "unplugin-vue-components/resolvers";
 import vue from "@vitejs/plugin-vue2";
+import viteCompression from "vite-plugin-compression";
+const OUTPUT_DIR = "dist";
 
 module.exports = defineConfig({
   resolve: {
@@ -18,8 +20,18 @@ module.exports = defineConfig({
     Components({
       resolvers: [VuetifyResolver()],
     }),
+    viteCompression({
+      algorithm: "gzip",
+      verbose: false,
+    }),
   ],
   server: {
     port: 4000,
+  },
+  build: {
+    target: "es2015",
+    cssTarget: "chrome80",
+    outDir: OUTPUT_DIR,
+    chunkSizeWarningLimit: 2000,
   },
 });
