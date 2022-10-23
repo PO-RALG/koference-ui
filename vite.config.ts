516023d840ec3fcc06368777d826bc8473c6e44d
Zachariah Ngonyani <zachariahn@gmail.com>
2022-10-23 12:24:41 +0300

    feat: add compresion and single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,32 @@
-import path from "path";
 import { defineConfig } from "vite";
 import Components from "unplugin-vue-components/vite";
 import { VuetifyResolver } from "unplugin-vue-components/resolvers";
 import vue from "@vitejs/plugin-vue2";
 import viteCompression from "vite-plugin-compression";
+import { loadEnv } from "vite";
+import { resolve } from "path";
+import dayjs from "dayjs";
+import pkg from "./package.json";
+import { viteSingleFile } from "vite-plugin-singlefile"
+
 const OUTPUT_DIR = "dist";
+const root = process.cwd();
+
+const { dependencies, devDependencies, name, version } = pkg;
+
+const __APP_INFO__ = {
+  pkg: { dependencies, devDependencies, name, version },
+  lastBuildTime: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+};
 
 module.exports = defineConfig({
+  base: "/",
+  root,
   resolve: {
     alias: [
       {
         find: "@",
-        replacement: path.resolve(__dirname, "src"),
+        replacement: resolve(__dirname, "src"),
       },
     ],
   },
@@ -21,9 +36,9 @@ module.exports = defineConfig({
       resolvers: [VuetifyResolver()],
     }),
     viteCompression({
-      algorithm: "gzip",
-      verbose: false,
+      compressionOptions: {},
     }),
+    viteSingleFile(),
   ],
   server: {
     port: 4000,
