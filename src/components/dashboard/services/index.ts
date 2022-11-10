f6ab7d115acfe17f1acca9fc6ff0251d048ccbbc
Zachariah Ngonyani <zachariahn@gmail.com>
2022-11-10 11:44:43 +0300

    add the rest of the dashboard apis in services

--- src/components/dashboard/services/index.ts
+++ src/components/dashboard/services/index.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
 
-const API = "/api/v1/dashboard/summary";
+const API = "/api/v1/dashboard";
 
 export const get = async (params: any) => {
-  return await axios.get(`${API}`, { params });
+  return await axios.get(`${API}/summary`, { params });
+};
+
+export const byMonth = async (params: any) => {
+  return await axios.get(`${API}/by-month`, { params });
+};
+
+export const getCarryoverByFundSource = async (params: any) => {
+  return await axios.get(`${API}/by-source-carryover`, { params });
+};
+
+export const getByCurrentSource = async (params: any) => {
+  return await axios.get(`${API}/by-source-current`, { params });
 };
