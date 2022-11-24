3dbf2bf7d6296ecaa4c1063f9c740e1e8f932ec9
mmnokote <mmnokote@gmail.com>
2022-11-24 09:20:44 +0300

    comment in mmama suppliers

--- src/components/payable/supplier/Supplier.vue
+++ src/components/payable/supplier/Supplier.vue
@@ -223,10 +223,10 @@
                     :error-messages="data.bankAccNumberError"
                   ></v-text-field>
                 </v-col>
-                <v-col cols="12" md="12" sm="12" class="pt-6">
+                <!-- <v-col cols="12" md="12" sm="12" class="pt-6">
                   <label for="">Tick if is M MAMA Supplier</label>
                   <v-checkbox v-model="data.formData.ismmama"></v-checkbox>
-                </v-col>
+                </v-col> -->
               </v-row>
             </v-container>
           </v-form>
